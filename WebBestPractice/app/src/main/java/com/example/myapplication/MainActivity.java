277084919a63148c24d6413f9c3385b6d0bf005c
Szc <2667127113@qq.com>
2019-07-09 10:16:59 +0800

    firstCommit

--- WebBestPractice/app/src/main/java/com/example/myapplication/MainActivity.java
+++ WebBestPractice/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,7 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.myapplication.Util.HttpCallbackListener;
 import com.example.myapplication.Util.HttpUtil;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +22,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //      获取到解析的数据
-        String response = HttpUtil.sendHttpRequest(address);
+//        String response = HttpUtil.sendHttpRequest(address);
+//      用HttpURLConnection返回网络请求
+        HttpUtil.sendHttpRequest(address, new HttpCallbackListener() {
+            @Override
+            public void onFinish(String response) {
+//                这里根据返回内容执行具体逻辑
+            }
+
+            @Override
+            public void onError(Exception e) {
+//                 连接失败  返回具体逻辑
+            }
+        });
+//      用okhttp返回网络请求
+        HttpUtil.sendOkhttpRequest(address, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+            }
+        });
+
 
     }
 }
--- WebBestPractice/app/src/main/java/com/example/myapplication/Util/HttpUtil.java
+++ WebBestPractice/app/src/main/java/com/example/myapplication/Util/HttpUtil.java
@@ -6,6 +6,9 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 public class HttpUtil {
     public static void sendHttpRequest(final String address, final HttpCallbackListener listener) {
         new Thread(new Runnable() {
@@ -45,5 +48,13 @@ public class HttpUtil {
                 }
             }
         }).start();
+    }
+
+    public static void sendOkhttpRequest(String address, okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                                     .url(address)
+                                     .build();
+        client.newCall(request).enqueue(callback);
     }
 }
