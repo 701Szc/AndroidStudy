21fb0fb19728f453cd1995145132e463aa59fb76
Szc <2667127113@qq.com>
2019-07-13 16:40:10 +0800

    隐藏系统标题栏、NestedScrollView

--- 10/MaterialTest/app/src/main/java/com/example/MaterialTest/MainActivity.java
+++ 10/MaterialTest/app/src/main/java/com/example/MaterialTest/MainActivity.java
@@ -6,28 +6,55 @@ import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
+    private Fruit[] fruits = {new Fruit("Apple",R.drawable.apple),
+                              new Fruit("Banana",R.drawable.banana),
+                              new Fruit("Orange",R.drawable.orange),
+                              new Fruit("Watermelon",R.drawable.watermelon),
+                              new Fruit("Pear",R.drawable.pear),
+                              new Fruit("Grape",R.drawable.grape),
+                              new Fruit("Pineapple",R.drawable.pineapple),
+                              new Fruit("strawberry",R.drawable.strawberry),
+                              new Fruit("Cherry",R.drawable.cherry),
+                              new Fruit("Mango",R.drawable.mango)};
+    private List<Fruit> fruitList = new ArrayList<>();
+    private FruitAdapter adapter;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initFruits();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recycle_view);
+        GridLayoutManager layoutManager = new GridLayoutManager(this,2);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new FruitAdapter(fruitList);
+        recyclerView.setAdapter(adapter);
+
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
@@ -56,6 +83,51 @@ public class MainActivity extends AppCompatActivity {
                 }).show();
             }
         });
+
+        swipeRefreshLayout = (SwipeRefreshLayout)findViewById(R.id.swipe_refresh);
+//        设置下拉刷新进度条的颜色
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshfruits();
+            }
+        });
+    }
+
+    private void refreshfruits() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Thread.sleep(2000);
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+//                在线程中对ui界面进行操作
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        initFruits();
+//                        通知数据发生了变化
+                        adapter.notifyDataSetChanged();
+//                        刷新事件结束，并隐藏刷新进度条
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                });
+            }
+        }).start();
+    }
+
+
+    private void initFruits() {
+        fruitList.clear();
+        for(int i = 0; i<30;i++){
+            Random random = new Random();
+            int index = random.nextInt(fruits.length);
+            fruitList.add(fruits[index]);
+        }
+
     }
 
     @Override
